f68390c03b2fc78e48795704824faf464eda73e8
Dmitry Tiulenev <lokkins334@gmail.com>
2021-07-06 23:04:52 +0500

    Some folder managment. Extended classes. And more

--- InputLanguageCharClass.py
+++
@@ -1,5 +0,0 @@
-class InputLanguageCharClass:
-    identifer = "ИДЕНТЕФИКАТОР"
-    equal = "РАВНО"
-    integer = "ЦЕЛОЕ"
-    semicolon = "тчкзпт"
--- Main.py
+++ Main.py
@@ -1,7 +1,6 @@
 import re
 
-#В этом классе хранятся все возможные классы отдельных символов, то есть если в блоке транслитерации мы видим букву то присваиваем ему letter и так далее.
-class StringCharClass:
+class StringCharClasses:
     letter = "буква"
     space = "пробел"
     equal = "равно"
@@ -12,7 +11,7 @@ class StringCharClass:
 
     #Этот метод определяет какой именно класс у символа, используем мы его в функции транслитерации
     def RecognizeCharacter(self, char):
-        stringChar = StringCharClass()
+        stringChar = StringCharClasses()
 
         if re.search(r'[а-яА-ЯёЁ]', char): return stringChar.error
 
@@ -25,32 +24,43 @@ class StringCharClass:
         
         return stringChar.error
 
+class InputLanguageCharClass:
+    identifer = "ИДЕНТЕФИКАТОР"
+    equal = "РАВНО"
+    integer = "ЦЕЛОЕ"
+    semicolon = "тчкзпт"
+
+    def IsPossibleIdentifierName(charClass, previousCharClass, resultTokenChain):
+        tokenArrayLength = len(resultTokenChain)
+        return charClass == previousCharClass or (tokenArrayLength >= 2 and resultTokenChain[tokenArrayLength - 2][0] == "const"
+        and previousCharClass == StringCharClasses.letter and charClass == StringCharClasses.digital)
+
+
 #Блок транслитерации, первый в списке
 def TransliterationBlock(str):
     #Создаем массив, в который будем складывать все символы с их классами и на 24 строке создаем экземпляр класса в котором находятся виды символов
     tokenChain = []
-    stringCharacters = StringCharClass()
+    stringChar = StringCharClasses()
     #Тут проходимся по символам переданой строки, в charClass пихаем класс текущего символа, на 28 в массив засовываем сам символ и его класс на одну позицию(это называется если че кортеж)
     for char in str:
-        charClass = stringCharacters.RecognizeCharacter(char)
+        charClass = stringChar.RecognizeCharacter(char)
         tokenChain.append((char, charClass))
     return tokenChain
 
 def LexicalBlock(tokenChain):
-    stringCharacters = StringCharClass()
-    resultTokenChain = [""]  
+    resultTokenChain = [("", "")]  
     previousCharClass = tokenChain[0][1]
 
     for char in tokenChain: 
-        if char[1] == previousCharClass:
-            resultTokenChain[len(resultTokenChain) - 1] += char[0]
+        if InputLanguageCharClass.IsPossibleIdentifierName(char[1], previousCharClass, resultTokenChain):
+            resultTokenChain[len(resultTokenChain) - 1][0] += char[0]
         
-        elif char[1] != stringCharacters.space:
-             resultTokenChain.append(char[0])
+        elif char[1] != StringCharClasses.space:
+             resultTokenChain.append((char[0], ""))
 
         previousCharClass = char[1]        
         
     return resultTokenChain
 
 
-print(TransliterationBlock(input()))
+print(LexicalBlock(TransliterationBlock(input())))
