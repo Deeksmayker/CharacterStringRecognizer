f9d8f5ee42e38c1b8bacf7bb165d17f0720a43cb
Dmitry Tiulenev <lokkins334@gmail.com>
2021-07-06 23:07:43 +0500

    Improve folder managment

--- TransliterationModule/StringCharTypes.py
+++ TransliterationModule/StringCharTypes.py
@@ -22,3 +22,15 @@ class StringCharClasses:
         if char == '+' or char == '-': return StringCharClasses.symbol
         
         return StringCharClasses.error
+
+
+#Блок транслитерации, первый в списке
+def TransliterationBlock(str):
+    #Создаем массив, в который будем складывать все символы с их классами и на 24 строке создаем экземпляр класса в котором находятся виды символов
+    tokenChain = []
+    stringChar = StringCharClasses()
+    #Тут проходимся по символам переданой строки, в charClass пихаем класс текущего символа, на 28 в массив засовываем сам символ и его класс на одну позицию(это называется если че кортеж)
+    for char in str:
+        charClass = stringChar.RecognizeCharacter(char)
+        tokenChain.append((char, charClass))
+    return tokenChain
