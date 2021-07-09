218a192ad4ac9386c48fd5806dd7f6586eb582ce
Dmitry Tiulenev <lokkins334@gmail.com>
2021-07-10 01:45:51 +0500

    Some recognizer work

--- TransliterationModule/StringCharTypes.py
+++ TransliterationModule/StringCharTypes.py
@@ -6,30 +6,20 @@ class StringCharClasses:
     equal = "равно"
     digital = "цифра"
     semicolon = "тчкзпт"
-    symbol = "знак"
+    sign = "знак"
     error = "ошибка"
+    bracket = "скобка"
+    squareBracket = "квадратная скобка"
 
     #Этот метод определяет какой именно класс у символа, используем мы его в функции транслитерации
     def RecognizeCharacter(char):
-    
-        if re.search(r'[а-яА-ЯёЁ]', char): return StringCharClasses.error
-
         if char == ' ': return StringCharClasses.space
         if char == ';': return StringCharClasses.semicolon
         if char == '=': return StringCharClasses.equal
-        if char.isalpha(): return StringCharClasses.letter
+        if char.isalpha() and re.search(r'[a-zA-Z]', char): return StringCharClasses.letter
         if char.isnumeric(): return StringCharClasses.digital
-        if char == '+' or char == '-': return StringCharClasses.symbol
+        if char == '+' or char == '-': return StringCharClasses.sign
+        if char == '(' or char == ')': return StringCharClasses.bracket
+        if char == '[' or char == ']': return StringCharClasses.squareBracket
         
         return StringCharClasses.error
-
-
-#Блок транслитерации, первый в списке
-def TransliterationBlock(str):
-    #Создаем массив, в который будем складывать все символы с их классами
-    tokenChain = []
-    #Тут проходимся по символам переданой строки, в charClass пихаем класс текущего символа, на 28 в массив засовываем сам символ и его класс на одну позицию(это называется если че кортеж)
-    for char in str:
-        charClass = StringCharClasses.RecognizeCharacter(char)
-        tokenChain.append((char, charClass))
-    return tokenChain
