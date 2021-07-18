983497009b5b5b2a8aa224d899257e4efd07549f
Dmitry Tiulenev <lokkins334@gmail.com>
2021-07-18 14:50:26 +0500

    Has 'finished' final recognizer

--- SyntaxBlockModule/AnalysisResults.py
+++ SyntaxBlockModule/AnalysisResults.py
@@ -1,3 +1,11 @@
 class AnalysisResults:
     correct = "ACCEPT"
     uncorrect = "REJECT"
+
+    def PrintOnCorrectLine():
+        print("Результат анализа - ", AnalysisResults.correct)
+        exit(0)
+
+    def PrintOnWrongLine():
+        print("Результат анализа - ", AnalysisResults.uncorrect)
+        exit(0)
